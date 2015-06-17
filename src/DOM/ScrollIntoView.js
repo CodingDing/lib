1f0355a784c2d922f4c1e3e7d43f54a06f030aad
ziki <251123397@qq.com>
2015-06-17 11:21:08 +0800

    scrollIntoView
    修改参数elem，兼容传字符串Id或者是Dom元素

--- src/DOM/ScrollIntoView.js
+++ src/DOM/ScrollIntoView.js
@@ -1,14 +1,21 @@
 
 var Core = (function(){
 
-
+/*
+*当隐藏元素在其offsetParent中不可见时自动滚动到可见区域
+*@parm {string|object} elem 需要被显示的元素
+*/
 function scrollToView(elem){
 
-		var offsetParent = elem.offsetParent;
+		var hide = typeof elem === 'string'
+					? document.getElementById(elem)
+					: elem;
+
+		var offsetParent = hide.offsetParent;
 
 		var scrollTop = offsetParent.scrollTop; //容器滚动条滚动高度
-		var offsetTop = elem.offsetTop; //隐藏元素上边界距offsetParent的高度
-		var h = elem.clientHeight; //隐藏元素的高度
+		var offsetTop = hide.offsetTop; //隐藏元素上边界距offsetParent的高度
+		var h = hide.clientHeight; //隐藏元素的高度
 		var H = offsetParent.clientHeight; //容器的高度
 
 		if(scrollTop > offsetTop){
